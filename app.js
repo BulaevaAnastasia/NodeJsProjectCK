981be2627fb27c7b5bdc4a35ffabf415d40fa7f8
bulaeva <bulaeva.exceedteam@gmail.com>
2021-11-03 17:12:27 +0300

    add file .env

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require("mongoose");
 const app = express();
+const dotenv = require('dotenv').config();
 
 const apiRoutes = require("./src/modules/routes/routes");
 
 app.use(cors());
 
-const uri = "mongodb+srv://bulaeva:Bulaeva_123@cluster0.li09f.mongodb.net/CoinKeeperDB?retryWrites=true&w=majority";
+const uri = process.env.DB_CONN;
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(bodyParser.json());
